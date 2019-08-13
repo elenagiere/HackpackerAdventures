8b6535326f289bdeca5dda2a0c50f420a65e10c6
Elena Giere <egiere@cisco.com>
2019-08-13 11:28:57 -0700

    silence dotenv errors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 let bodyParser = require('body-parser');
 const Mailchimp = require('mailchimp-api-v3');
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config({silent: true});
 
 var mc_api_key = process.env.REACT_APP_MAILCHIMP_API_KEY;
 var list_id = process.env.REACT_APP_MAILING_LIST_ID;
